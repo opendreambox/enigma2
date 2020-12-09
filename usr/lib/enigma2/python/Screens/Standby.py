c641f03cc7e0777706919e1ef14de3628b3c4166
dream-alpha <swmaniacster@gmail.com>
2020-12-09 14:31:48 +0100

    handle jobs and timers sequentially: first wait for completion of jobs and then handle timers

--- usr/lib/enigma2/python/Screens/Standby.py
+++ usr/lib/enigma2/python/Screens/Standby.py
@@ -121,7 +121,7 @@ class StandbySummary(Screen):
 		</widget>
 	</screen>"""
 
-from enigma import quitMainloop, iRecordableService
+from enigma import quitMainloop, iRecordableService, eTimer
 from Screens.MessageBox import MessageBox
 from time import time
 from Components.Task import job_manager
@@ -129,45 +129,90 @@ from Components.Task import job_manager
 inTryQuitMainloop = False
 
 class TryQuitMainloop(MessageBox):
-	def __init__(self, session, retvalue=1, timeout=-1, default_yes = True):
-		self.retval=retvalue
-		recordings = session.nav.getRecordings()
-		jobs = len(job_manager.getPendingJobs())
-		self.connected = False
-		reason = ""
-		next_rec_time = -1
-		if not recordings:
-			next_rec_time = session.nav.RecordTimer.getNextRecordingTime()	
-		if recordings or (next_rec_time > 0 and (next_rec_time - time()) < 360):
-			reason = _("Recording(s) are in progress or coming up in few seconds!") + '\n'
-		if jobs:
-			if jobs == 1:
-				job = job_manager.getPendingJobs()[0]
-				reason += "%s: %s (%d%%)\n" % (job.getStatustext(), job.name, int(100*job.progress/float(job.end)))
-			else:
-				reason += (_("%d jobs are running in the background!") % jobs) + '\n'
+	def __init__(self, session, retvalue=1, timeout=-1, default_yes=True):
+		self.session = session
+		self.retval = retvalue
+		self.check_jobs_timer = eTimer()
+		self.check_jobs_timer_conn = self.check_jobs_timer.timeout.connect(self.updateJobs)
+		self.connected_timers = False
+		self.connected_jobs = False
+		self.question = ""
+
+		reason = self.checkJobs()
+		if reason:
+			self.check_jobs_timer.start(5000, True)
+			self.connected_jobs = True
+		else:
+			reason = self.checkTimers()
+			if reason:
+				self.connected_timers = True
+
 		if retvalue == 16:
-			reason += _("Really reboot into Recovery Mode?\n")
+			reason = _("Really reboot into Recovery Mode?\n")
 		if reason:
 			if retvalue == 1:
-				MessageBox.__init__(self, session, reason+_("Really shutdown now?"), type = MessageBox.TYPE_YESNO, timeout = timeout, default = default_yes)
+				self.question = _("Really shutdown now?")
+				reason += self.question
+				MessageBox.__init__(self, session, reason, type=MessageBox.TYPE_YESNO, timeout=timeout, default=default_yes)
 			elif retvalue == 2:
-				MessageBox.__init__(self, session, reason+_("Really reboot now?"), type = MessageBox.TYPE_YESNO, timeout = timeout, default = default_yes)
+				self.question = _("Really reboot now?")
+				reason += self.question
+				MessageBox.__init__(self, session, reason, type=MessageBox.TYPE_YESNO, timeout=timeout, default=default_yes)
 			elif retvalue == 4:
 				pass
 			elif retvalue == 16:
-				MessageBox.__init__(self, session, reason+_("You won't be able to leave Recovery Mode without physical access to the device!"), type = MessageBox.TYPE_YESNO, timeout = timeout, default = default_yes)
+				self.question = _("You won't be able to leave Recovery Mode without physical access to the device!")
+				reason += self.question
+				MessageBox.__init__(self, session, reason, type=MessageBox.TYPE_YESNO, timeout=timeout, default=default_yes)
 			else:
-				MessageBox.__init__(self, session, reason+_("Really restart now?"), type = MessageBox.TYPE_YESNO, timeout = timeout, default = default_yes)
+				self.question = _("Really restart now?")
+				reason += self.question
+				MessageBox.__init__(self, session, reason, type=MessageBox.TYPE_YESNO, timeout=timeout, default=default_yes)
 			self.skinName = "MessageBox"
 			session.nav.record_event.append(self.getRecordEvent)
-			self.connected = True
 			self.onShow.append(self.__onShow)
 			self.onHide.append(self.__onHide)
 		else:
 			self.skin = """<screen name="TryQuitMainloop" position="0,0" size="0,0" flags="wfNoBorder"/>"""
 			Screen.__init__(self, session)
 			self.close(True)
+
+	def checkJobs(self):
+		reason = ""
+		jobs = len(job_manager.getPendingJobs())
+		if jobs:
+			if jobs == 1:
+				job = job_manager.getPendingJobs()[0]
+				reason += "%s: %s (%d%%)\n" % (job.getStatustext(), job.name, int(100 * job.progress / float(job.end)))
+			else:
+				reason += (_("%d jobs are running in the background!") % jobs) + '\n'
+		return reason
+
+	def checkTimers(self):
+			reason = ""
+			next_rec_time = -1
+			recordings = self.session.nav.getRecordings()
+			if not recordings:
+				next_rec_time = self.session.nav.RecordTimer.getNextRecordingTime()
+			if recordings or (next_rec_time > 0 and (next_rec_time - time()) < 360):
+				reason = _("Recording(s) are in progress or coming up in few seconds!") + '\n'
+			return reason
+
+	def updateJobs(self):
+		reason = self.checkJobs()
+		if reason:
+			self["text"].setText(reason + self.question)
+			self.check_jobs_timer.start(5000, True)
+		else:
+			self.connected_jobs = False
+			reason = self.checkTimers()
+			if reason:
+				self["text"].setText(reason + self.question)
+				self.session.nav.record_event.append(self.getRecordEvent)
+			else:
+				self.skin = """<screen name="TryQuitMainloop" position="0,0" size="0,0" flags="wfNoBorder"/>"""
+				Screen.__init__(self, self.session)
+				self.close(True)
 
 	def getRecordEvent(self, recservice, event):
 		if event == iRecordableService.evEnd:
@@ -183,13 +228,26 @@ class TryQuitMainloop(MessageBox):
 			self.stopTimer()
 
 	def close(self, value):
-		if self.connected:
-			self.conntected=False
-			self.session.nav.record_event.remove(self.getRecordEvent)
-		if value:
-			quitMainloop(self.retval)
-		else:
+		if not value:
 			MessageBox.close(self, True)
+		else:
+			if self.connected_jobs:
+				self.connected_jobs = False
+				self.check_jobs_timer.stop()
+				for job in job_manager.getPendingJobs():
+					job.abort()
+				reason = self.checkTimers()
+				if reason:
+					self["text"].setText(reason + self.question)
+					self.session.nav.record_event.append(self.getRecordEvent)
+					self.connected_timers = True
+				else:
+					quitMainloop(self.retval)
+			else:
+				if self.connected_timers:
+					self.connected_timers = False
+					self.session.nav.record_event.remove(self.getRecordEvent)
+				quitMainloop(self.retval)
 
 	def __onShow(self):
 		global inTryQuitMainloop
